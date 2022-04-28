fb61fa01f33a3fc1d3c202601410f0cb23f4d641
aq032 <anthonyq032@gmail.com>
2022-04-27 20:57:32 -0700

    Optimized "!choose" command

--- cogs/greetings.py
+++ cogs/greetings.py
@@ -1,8 +1,10 @@
+import random
+
 from discord.ext import commands
 from discord.ext.commands.context import Context
 
 from .base import CommandBase
-import random
+
 
 class Greetings(CommandBase):
     @commands.command()
@@ -22,12 +24,7 @@ class Greetings(CommandBase):
         await ctx.channel.send("L")
 
     @commands.command()
-    async def choose(self, ctx: Context, *args):
+    async def choose(self, ctx: Context, *args: str):
         # Sends a message to the channel using the Context object.
-        # need to fix separation of words counting as different arguments
-        # should separate by commas
-        join_words = " ".join(args)
-        join_words = join_words.split(",")
-
-        random_integer = random.randrange(len(join_words))
-        await ctx.channel.send(join_words[random_integer])
+        join_words = " ".join(args).split(",")
+        await ctx.channel.send(random.choice(join_words))
