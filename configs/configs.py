a0bb159875562efe1f7c3e190209280053813f14
Ivan <ivanfurlong@gmail.com>
2022-06-09 16:15:42 -0700

    Changed configs

---
+++ configs/configs.py
@@ -0,0 +1,11 @@
+
+# Discord Bot Settings
+DISCORD_TOKEN = ""
+
+# Event listeners
+ENABLE_SOCIAL_CREDIT = False
+UPVOTE_EMOJI_NAME = None
+DOWNVOTE_EMOJI_NAME = None
+SERVER_BLACKLIST = {}
+
+# Misc Settings
