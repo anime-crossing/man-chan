802b11bb8b05cff90d9310ae962bc6712b18baf7
manuelgavino16 <manuelgavino16@gmail.com>
2022-12-13 22:56:06 -0700

    fixed pr comments

--- cogs/shareLink.py
+++ cogs/shareLink.py
@@ -1,5 +1,5 @@
 import logging
-import random
+from typing import Any, Dict, cast
 
 import spotipy
 from discord.ext import commands
@@ -16,8 +16,8 @@ class ShareLink(CommandBase):
         super().__init__(bot)
         self.sp = spotipy.Spotify(
             auth_manager=SpotifyClientCredentials(
-                client_id=self.configs["CLIENT_ID"],
-                client_secret=self.configs["CLIENT_SECRET"],
+                client_id=self.configs["SPOTIFY_CLIENT_ID"],
+                client_secret=self.configs["SPOTIFY_CLIENT_SECRET"],
             )
         )
 
@@ -31,15 +31,21 @@ class ShareLink(CommandBase):
 
     @commands.command()
     async def album(self, ctx: Context, *args: str):
-        join_words = " ".join(args)
+        join_words = " ".join(args).strip()
         results = self.sp.search(q=join_words, limit=1, type="album")
         await ctx.channel.send(
             results["albums"]["items"][0]["external_urls"]["spotify"]
         )
 
+    @classmethod
+    def is_enabled(cls, configs: Dict[str, Any] = {}):
+        return (
+            configs["ENABLE_SHARE_LINK"]
+        )
+
 
 async def setup(bot: ManChanBot):
-    if ShareLink.is_enabled():
+    if ShareLink.is_enabled(bot.configs):
         await bot.add_cog(ShareLink(bot))  # type: ignore
     else:
         logging.warn("SKIPPING: cogs.sharelink")
