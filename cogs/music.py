6a0a8ca93bd646d5fe27388fb86921857dfedeb9
Ivan <ivanfurlong@gmail.com>
2022-12-14 22:28:44 -0800

    added more ytdl logic

--- cogs/music.py
+++ cogs/music.py
@@ -1,16 +1,18 @@
 from discord.ext import commands
 from discord.ext.commands.context import Context
 from .commandbase import CommandBase
-from typing import Any, Dict, cast
-from discord import User, Member, VoiceState, VoiceChannel
-from utils.context import get_member, get_author_voice_channel
-import youtube_dl
+from typing import cast
+from discord import VoiceClient
+from utils.context import get_author_voice_channel
+from main import ManChanBot
+from utils.yt_dl import YTDHandler
+import logging
 
 
 class MusicPlayer(CommandBase):
 
     @commands.command()
-    async def play(self, ctx: Context):
+    async def play(self, ctx: Context, *args: str):
         channel = get_author_voice_channel(ctx)
 
         if not channel:
@@ -18,8 +20,30 @@ class MusicPlayer(CommandBase):
         self.bot.loop
 
         await channel.connect()
+        if len(args) == 0:
+            return
+
+        file = await YTDHandler.from_url(ctx, args[0], self.bot.loop)
+
+        print("CLASS:", ctx.voice_client.__class__)
+        # voice_channel.play
+        vc = cast(VoiceClient, ctx.voice_client)
+        # channel.voice_state
+        vc.play(file)
 
 
     @commands.command()
     async def leave(self, ctx: Context):
-        pass
+        voice_client = ctx.voice_client
+        print(voice_client, voice_client.channel)
+        if not voice_client or not voice_client.channel:
+            return await ctx.send('Not connected to any voice channel.')
+        
+        await voice_client.disconnect(force=True)
+
+
+async def setup(bot: ManChanBot):
+    if MusicPlayer.is_enabled(bot.configs):
+        await bot.add_cog(MusicPlayer(bot))  # type: ignore
+    else:
+        logging.warn("SKIPPING: cogs.socialcredit")
--- utils/yt_dl.py
+++ utils/yt_dl.py
@@ -1,27 +1,92 @@
-import youtube_dl
-from discord import PCMVolumeTransformer, AudioSource
-from asyncio import get_event_loop, BaseEventLoop, AbstractEventLoop
+# # # #
+# https://gist.github.com/vbe0201/ade9b80f2d3b64643d854938d40a0a2d
+# https://medium.com/pythonland/build-a-discord-bot-in-python-that-plays-music-and-send-gifs-856385e605a1
+# # # #
 
-from typing import Optional
+import youtube_dl
+from discord import PCMVolumeTransformer, FFmpegPCMAudio
+from asyncio import get_event_loop, AbstractEventLoop
+
+from typing import Optional, cast
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+FFMPEG_OPTIONS = {
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+    'options': '-vn',
+}
 
 youtube_dl.utils.bug_reports_message = lambda: ''
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
 class YTDHandler(PCMVolumeTransformer):
 
-    def __init__(self, original: AudioSource, data, volume: float=0.5):
-        super().__init__(original, volume)
+    def __init__(self, ctx, source: FFmpegPCMAudio, data: dict, volume: float=0.5):
+        super().__init__(source, volume)
 
         self._data = data
-        self._url = ''
+        self.requester = ctx.author
+        self.channel = ctx.channel
+        self.data = data
+
+        self.uploader = data.get('uploader')
+        self.uploader_url = data.get('uploader_url')
+        date = data.get('upload_date')
+        self.upload_date = date[6:8] + '.' + date[4:6] + '.' + date[0:4]
+        self.title = data.get('title')
+        self.thumbnail = data.get('thumbnail')
+        self.description = data.get('description')
+        # self.duration = self.parse_duration(int(data.get('duration')))
+        self.tags = data.get('tags')
+        self.url = data.get('webpage_url')
+        self.views = data.get('view_count')
+        self.likes = data.get('like_count')
+        self.dislikes = data.get('dislike_count')
+        self.stream_url = data.get('url')
 
     @classmethod
-    async def from_url(cls, url: str, *, loop: Optional[AbstractEventLoop] = None, stream=False) -> str:
+    async def from_url(cls, ctx, url: str, loop: Optional[AbstractEventLoop] = None, stream=False) -> 'YTDHandler':
         loop = loop or get_event_loop()
 
         
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-        if 'entries' in data:
-            # take first item from a playlist
-            data = data['entries'][0]
-        filename = data['title'] if stream else ytdl.prepare_filename(data)
-        return filename
+        data: dict = cast(dict, await loop.run_in_executor(
+            None, 
+            lambda: ytdl.extract_info(url, download=False)))
+
+        if data is None:
+            return ''
+
+        if "entries" not in data:
+            process_info = data
+        else:
+            process_info = None
+            for entry in data["entries"]:
+                if entry:
+                    process_info = entry
+
+        if process_info == None:
+            return ''
+
+        webpage_url = process_info["webpage_url"]
+        url = process_info["url"]
+
+        print("RESULTS:", webpage_url, url)
+
+        
+        # if 'entries' in data:
+        #     # take first item from a playlist
+        #     data = data['entries'][0]
+        # filename = data['title'] if stream else ytdl.prepare_filename(data)
+        # return filename
+        return cls(ctx, FFmpegPCMAudio(url, **FFMPEG_OPTIONS), data=process_info)
