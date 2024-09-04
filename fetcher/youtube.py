9629d02b9feb1121e4340773f1ccbb16a8f25825
manuelgavino16 <manuelgavino16@gmail.com>
2024-09-03 22:44:13 -0700

    hotfix for urls with timestamp in them

--- fetcher/youtube.py
+++ fetcher/youtube.py
@@ -3,6 +3,7 @@ from typing import Any
 from yt_dlp import YoutubeDL
 
 from models import Song
+from urllib.parse import parse_qs, urlsplit, urlunsplit
 
 
 class YoutubeApi:
@@ -14,8 +15,16 @@ class YoutubeApi:
 
     @classmethod
     def search(cls, query: str, limit=1) -> list[Song]:
+        parsed_search = urlsplit(query)
+        if (all([parsed_search.scheme, parsed_search.netloc])):
+            queries = parse_qs(parsed_search.query)
+            queries.pop('t', None)
+            queries_url = "&".join(["{}={}".format(key, value[0]) for key, value in queries.items()])
+            parsed_search = urlunsplit((parsed_search.scheme, parsed_search.netloc, parsed_search.path, queries_url, parsed_search.fragment))
+        else:
+            parsed_search = urlunsplit(parsed_search)
         results: Any = YoutubeDL(cls.YDL_OPTIONS).extract_info(
-            "ytsearch%d:%s" % (limit, query), download=False
+            "ytsearch%d:%s" % (limit, parsed_search), download=False
         )
         songs = []
         for entry in results["entries"]:
